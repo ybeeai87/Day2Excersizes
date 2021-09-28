f856f7355cd151e59d3aa955810f8b8918a3787f
Yousif <ybeeai@gmail.com>
2021-09-28 14:08:08 -0400

    second commit

--- Day2Exercise/Program.cs
+++ Day2Exercise/Program.cs
@@ -17,6 +17,26 @@ namespace Day2Exercise
                     Console.WriteLine(i);
                 }
             }
+            /* Create a bool variable named runProgram = true
+            Create a while loop that uses runProgram as the condition
+            inside the loop ask if they want to continue looping.
+            if yes then loop again
+            if no stop loop*/
+            bool runProgram = true;
+
+            while(runProgram)
+            {
+                
+
+                Console.WriteLine("Do you want to continue looping? (y/n)");
+                string choice = Console.ReadLine();
+                if (choice == "n")
+                {
+                    runProgram = false;
+                }
+
+
+            } 
 
         }
     }
